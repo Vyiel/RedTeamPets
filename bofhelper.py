b112959f6bfe2d5db16f2ba8f7aa7d58f46d497c
Budhaditya Bose <48716228+Vyiel@users.noreply.github.com>
2022-06-09 04:07:00 +0530

    Buffer Overflow Offset Recognition
    Just a simple custom made python BOF offset recognition tool coz I don't like remembering MSF commands

---
+++ bofhelper.py
@@ -0,0 +1,50 @@
+def generate(rlen):
+	letters = string.ascii_letters
+	return ''.join(random.choice(letters) for i in range(int(rlen)))
+
+def offset(hex, pattern):
+	hexed = hex
+	ascii = bytearray.fromhex(hexed).decode()
+	loc = pattern.find(ascii)
+	return loc
+
+
+import random
+import sys
+import string
+
+print("""
+######                                                             #     # ####### ### #       
+#     # #####   ####  ###### ######  ####   ####   ####  #####     #     # #        #  #       
+#     # #    # #    # #      #      #      #      #    # #    #    #     # #        #  #       
+######  #    # #    # #####  #####   ####   ####  #    # #    #    #     # #####    #  #       
+#       #####  #    # #      #           #      # #    # #####      #   #  #        #  #       
+#       #   #  #    # #      #      #    # #    # #    # #   #       # #   #        #  #       
+#       #    #  ####  #      ######  ####   ####   ####  #    #       #    ####### ### ####### 
+""")
+print()
+print()
+
+choice = input("Pattern or Static Character? P/S: ")
+if choice == "S":
+	iteration = int(input("Enter number of times you need to print A: "))
+	print(str('A' * int(iteration)))
+
+elif choice == "P":
+
+	length = int(input("Enter Length of pattern: "))
+	pattern = str(generate(length))
+	print(pattern)
+	print()
+	
+	while True:
+		try:
+			f_offset = input("Enter overwritten IP value in hex: ")
+			print(f_offset)
+			print (offset(f_offset, pattern))
+			break
+		except:
+			print("Try Again! ")
+			continue
+
+	
